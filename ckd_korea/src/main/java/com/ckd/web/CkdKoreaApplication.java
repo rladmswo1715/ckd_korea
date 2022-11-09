b265a0cd827720debf42bbed48b439b8c0b41b5d
rladmswo1715 <rladmswo1715@naver.com>
2022-11-09 17:51:04 +0900

    create project

---
+++ ckd_korea/src/main/java/com/ckd/web/CkdKoreaApplication.java
@@ -0,0 +1,13 @@
+package com.ckd.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CkdKoreaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CkdKoreaApplication.class, args);
+	}
+
+}
---
+++ ckd_korea/src/test/java/com/ckd/web/CkdKoreaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ckd.web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CkdKoreaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
